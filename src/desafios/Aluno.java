9a6b5254ffac84597441ece6877354a9d7b00c3a
Lucas <lucaspereiradelima2020@gmail.com>
2025-11-09 20:37:37 -0300

    Criação da Classe Aluno com seus métodos e atributos.

---
+++ src/desafios/Aluno.java
@@ -0,0 +1,19 @@
+/**
+ * Crie uma classe Aluno com atributos nome, idade,
+ * e um método para exibir informações.
+ * Crie uma instância da classe Aluno, atribua valores aos seus atributos
+ * e utilize o método para exibir as informações.
+ */
+
+package desafios;
+
+public class Aluno {
+
+    String nome;
+    int idade;
+
+    public void exibeInformacoesDoAluno() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+    }
+}
