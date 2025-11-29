a994a091009d70728847cef62e01a21a2a958ad9
Lucas <lucaspereiradelima2020@gmail.com>
2025-11-29 19:31:29 -0300

    Classe IdadePessoa

---
+++ src/desafios/desafio02/IdadePessoa.java
@@ -0,0 +1,31 @@
+package desafios.desafio02;
+
+public class IdadePessoa {
+
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void verificarIdade() {
+        if (idade >= 18) {
+            System.out.println(String.format("O %s tem %d anos, então ele é maior de idade.", getNome(), getIdade()));
+        } else {
+            System.out.println(String.format("O %s tem %d anos, então ele é maior de idade.", getNome(), getIdade()));
+        }
+    }
+}
