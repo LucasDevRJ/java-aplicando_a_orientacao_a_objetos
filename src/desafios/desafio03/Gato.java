9be3f9c4efbf23c070ec3cd1709394223a05f5f9
Lucas <lucaspereiradelima2020@gmail.com>
2025-12-19 17:25:17 -0300

    Exercício de aplicação de herenaça e polimorfismo usando Animais como exemplo.

---
+++ src/desafios/desafio03/Gato.java
@@ -0,0 +1,13 @@
+package desafios.desafio03;
+
+public class Gato extends Animal {
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Miau");
+    }
+
+    void arranharMoveis() {
+        System.out.println("Arranhando moveis");
+    }
+}
--- src/desafios/desafio03/Principal.java
+++ src/desafios/desafio03/Principal.java
@@ -7,5 +7,18 @@ public class Principal {
         modeloCarro.definirPrecosMediosDosTresAnos(142000, 144000, 141000);
         modeloCarro.exibirMaiorPrecoMedio();
         modeloCarro.exibirMenorPrecoMedio();
+
+        System.out.println("-------------------------------");
+
+        Animal animal = new Animal();
+        animal.emitirSom();
+
+        Cachorro cachorro = new Cachorro();
+        cachorro.emitirSom();
+        cachorro.abanarRabo();
+
+        Gato gato = new Gato();
+        gato.emitirSom();
+        gato.arranharMoveis();
     }
 }
